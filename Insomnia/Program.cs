3b7549b1d8b2d0d3f33cc4a450569149ac864b8f
calguymike <69073399+calguymike@users.noreply.github.com>
2020-11-10 22:08:33 +0000

    Insomnia
    V1

---
+++ Insomnia/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Timers;
+
+namespace Insomnia
+{
+
+    class Program
+    {
+        private static System.Timers.Timer aTimer;
+        [DllImport("kernel32.dll")]
+        static extern EXECUTION_STATE SetThreadExecutionState(EXECUTION_STATE esFlags);
+
+        [FlagsAttribute]
+        enum EXECUTION_STATE : uint
+        {
+            ES_AWAYMODE_REQUIRED = 0x00000040,
+            ES_CONTINUOUS = 0x80000000,
+            ES_DISPLAY_REQUIRED = 0x00000002,
+            ES_SYSTEM_REQUIRED = 0x00000001
+        }
+
+        static void Main(string[] args)
+        {
+            SetTimer();
+
+            Console.WriteLine("\nPress the Enter key to exit the application...\n");
+            Console.WriteLine("Mischief started at {0:HH:mm:ss.fff}", DateTime.Now);
+            Console.ReadLine();
+            aTimer.Stop();
+            aTimer.Dispose();
+
+            Console.WriteLine("Mischief managed...");
+
+        }
+
+        private static void SetTimer()
+        {
+            
+            aTimer = new System.Timers.Timer(60000);
+            aTimer.Elapsed += OnTimedEvent;
+            aTimer.AutoReset = true;
+            aTimer.Enabled = true;
+        }
+
+        private static void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            Console.WriteLine("Reset",
+                              e.SignalTime);
+            Disable();
+        }
+
+
+        public static void Disable()
+        {
+            SetThreadExecutionState(
+                EXECUTION_STATE.ES_DISPLAY_REQUIRED | EXECUTION_STATE.ES_CONTINUOUS);
+        }
+
+        public static void Enable()
+        {
+            SetThreadExecutionState(EXECUTION_STATE.ES_CONTINUOUS);
+        }
+
+    }
+}
